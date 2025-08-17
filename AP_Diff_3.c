6dd4222303c52afb10008ad4d37570277877fa51
vineet1cg <vineet.prajapati.cg@gmail.com>
2025-08-17 23:40:39 +0530

    Answered Assignment 8 ;)

--- AP_Diff_3.c
+++ AP_Diff_3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main (){
     int n;
+    
     printf("Enter Numbers Of Tearms : ");
     scanf("%d",&n);
     for(int i= 4;i<=(3*n)+1;i=i+3){
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Part Of My Assignment *--*
 I Learned Loops In C Lang ;)
 Git Helps To Track Changes In My Code.
+This Is For The Ans Of Assign 8 (*_*)
